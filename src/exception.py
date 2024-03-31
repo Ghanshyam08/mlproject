0fdbe937fa1cfb2a478ddb08247ff26bb47fc0af
Ghanshyam08 <jetanighanshyam9@gmail.com>
2024-03-31 16:56:17 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -20,11 +20,3 @@ class CustomException(Exception):
         return self.error_message
     
 
-if __name__=="__main__":
-
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("divide by 0")
-        raise CustomException(e,sys)
-    
